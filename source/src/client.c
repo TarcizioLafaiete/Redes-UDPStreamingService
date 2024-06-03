4d25e1fa2f0f805664632eebc220ba9b9fe1ff08
TarcizioLafaiete <tarcizio-augusto@hotmail.com>
2024-06-03 14:38:04 -0300

    test: Testanto primeiro procedimento de comunicação

--- source/src/client.c
+++ source/src/client.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "../include/socketUtils.h"
 #include "../include/clientCore.h"
@@ -16,12 +17,27 @@ clientCore initClient(char* argv[]){
     core.serverSocket = configure_addr(core.inet,atoi(argv[3]));
     
     connect_client(&core);
+    printf("connect socket \n");
 
     return core;
 }
 
 int main(int argc, char* argv[]){
+
+    printf("Iniciando esta merda \n");
+
     clientCore core = initClient(argv);
+
+    datagram msg = {.id = 1, .timeStamp = (int)time(NULL), .buffer = "Hello Server \n"};
+
+    printf("Sending datagram \n");
+    sendto(core.client_fd,&msg,500,0,(struct sockaddr*)NULL,sizeof(core.serverSocket.addr));
+    printf("Datagrama enviado \n");
+
+    datagram ret;
+    recvfrom(core.client_fd,&ret,sizeof(ret),0,(struct sockaddr*)NULL,NULL);
+
+    printf("id: %d timestamp: %d buffer: %s \n",ret.id,ret.timeStamp,ret.buffer);
 
     return 0;
 }
--- source/src/server.c
+++ source/src/server.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "../include/socketUtils.h"
 #include "../include/serverCore.h"
@@ -11,19 +12,36 @@ serverCore initServer(char* argv[]){
 
     core.inet = translateIP(argv[1]);
 
+    printf("IP Translated \n");
     create_socket(&core.server_fd,core.inet);
+    printf("create socket \n");
     core.socket = configure_addr(core.inet,atoi(argv[2]));
+    printf("configure addr \n");
     configure_options(core.server_fd);
+    printf("configure addr \n");
     binding(&core);
+    printf("binding \n");
     return core;
 }
 
 int main(int argc,char* argv[]){
 
+    printf("Iniciando essa merda \n");
+
     serverCore core = initServer(argv);
 
-    while(1){
-    }
+    printf("Esperando datagrama \n");
+    datagram rec;
+    struct sockaddr cliaddr;
+    int len = sizeof(cliaddr);
+    int n = recvfrom(core.server_fd,&rec,sizeof(rec),0,&cliaddr,&len);
+    printf("datagrama recebido \n");
+    
+    datagram sen = {.id = 2, .timeStamp = (int)time(NULL),.buffer = "Hello Client \n"};
+    sendto(core.server_fd,&sen,500,0,(struct sockaddr*)&cliaddr,sizeof(cliaddr));
+
+    printf("id: %d timestamp: %d buffer: %s \n",rec.id,rec.timeStamp,rec.buffer);
+
 
     return 0;
 }
