4db2ee1956390cb2432f5daff979d73b620f6f29
Tarcizio Lafaiete <tarcizio-augusto@hotmail.com>
2024-06-04 01:03:03 -0300

    refactor: Convertendo as filas em modelos genericos

--- source/include/queue.h
+++ source/include/queue.h
@@ -1,13 +1,12 @@
 #ifndef __QUEUE__
 #define __QUEUE__
 
-#include "socketUtils.h"
 
 typedef struct queue S_Queue;
 
 S_Queue* create_queue();
-void enqueue(S_Queue* queue,datagram data);
-datagram dequeue(S_Queue* queue);
+void enqueue(S_Queue* queue,void* data);
+void* dequeue(S_Queue* queue);
 int isEmpty(S_Queue* queue);
 
 
--- source/src/queue.c
+++ source/src/queue.c
@@ -1,41 +1,47 @@
 #include "../include/queue.h"
 
-typedef struct{
-    datagram data;
-    cell* next;
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct cell{
+    void* data;
+    struct cell* next;
 } cell;
 
 typedef struct queue{
     cell* head;
     cell* tail;
     int size;
+    size_t sizeofItem;
 } S_Queue;
 
-S_Queue* create_queue(){
+S_Queue* create_queue(int sizeofData){
     S_Queue* queue = (S_Queue*) malloc(sizeof(S_Queue));
 
 
     queue->head = (cell*)malloc(sizeof(cell));
     queue->tail = queue->head;
     queue->size = 0;
+    queue->sizeofItem = sizeofData;
     
     return queue;
 }
 
-void enqueue(S_Queue* queue,datagram data){
+void enqueue(S_Queue* queue,void* data){
     cell* newCell = (cell*) malloc(sizeof(cell));
     
-    newCell->data = data;
+    memcpy(newCell->data,data,queue->sizeofItem);
     queue->tail->next = newCell;
     queue->tail = newCell;
     queue->size++;
 }
 
-datagram dequeue(S_Queue* queue){
+void* dequeue(S_Queue* queue){
     cell* auxCell;
-    datagram data;
+    void* data;
     
-    data = queue->head->next->data;
+    // data = queue->head->next->data;
+    memcpy(data,queue->head->next->data,queue->sizeofItem);
     auxCell = queue->head;
     queue->head = queue->head->next;
     free(auxCell);
