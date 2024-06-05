e660660c51af30c670f07458221f614a45fd808e
Tarcizio Lafaiete <tarcizio-augusto@hotmail.com>
2024-06-04 21:49:58 -0300

    build: Mudando regra de build de server

--- source/compile.sh
+++ source/compile.sh
@@ -1,2 +1,2 @@
-gcc src/server.c src/socketUtils.c src/serverCore.c src/vector.c -o server
+gcc src/server.c src/socketUtils.c src/serverCore.c src/linked_list.c -o server
 gcc src/client.c src/socketUtils.c src/clientCore.c src/terminalPrinter.c -o client
