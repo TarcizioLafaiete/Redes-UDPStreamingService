eb4be5828a8cf5143c0ed7fea4cafcd26abf9eac
TarcizioLafaiete <tarcizio-augusto@hotmail.com>
2024-06-10 12:22:12 -0300

    feat: Adicionando scripts dos filmes a serem enviados

---
+++ source/include/movie_script.h
@@ -0,0 +1,24 @@
+#ifndef __MOVIE_SCRIPT__
+#define __MOVIE_SCRIPT__
+
+#define LORD_OF_RINGS 0
+#define GODFATHER 1
+#define FIGHT_CLUB 2
+
+const char* movie_script[3][5] = {{"Um anel para a todos governar" , 
+                                "Na terra de Mordor onde as sombras se deitam",
+                                "Nao e o que temos, mas o que fazemos com o que temos",
+                                "Nao há mal que sempre dure",
+                                "O mundo esta mudando, senhor Frodo"},
+                                {"Vou fazer uma oferta que ele nao pode recusar",
+                                "Mantenha seus amigos por perto e seus inimigos mais perto ainda",
+                                "E melhor ser temido que amado",
+                                "A vinganca e um prato que se come frio",
+                                "Nunca deixe que ninguem saiba o que você esta pensando"},
+                                {"Primeira regra do Clube da Luta: voce nao fala sobre o Clube da Luta",
+                                "Segunda regra do Clube da Luta: voce nao fala sobre o Clube da Luta",
+                                "O que voce possui acabara possuindo voce",
+                                "E apenas depois de perder tudo que somos livres para fazer qualquer coisa",
+                                "Escolha suas lutas com sabedoria"}};
+
+#endif
